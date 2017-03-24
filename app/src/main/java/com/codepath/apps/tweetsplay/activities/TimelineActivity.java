c04060654ac385059fc4cdb8c61faa45c109bfca
eratkarpriyanka <eratkar.priyanka89@gmail.com>
2017-03-24 16:49:43 -0700

    Adding new files

---
+++ app/src/main/java/com/codepath/apps/tweetsplay/activities/TimelineActivity.java
@@ -0,0 +1,15 @@
+package com.codepath.apps.tweetsplay.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codepath.apps.tweetsplay.R;
+
+public class TimelineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/tweetsplay/network/IWSResponseListener.java
@@ -0,0 +1,8 @@
+package com.codepath.apps.tweetsplay.network;
+
+
+public interface IWSResponseListener {
+
+
+}
+
